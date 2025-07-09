dbdf23378750138b5c466a42ca53d38fd51ef7b4
Romain Gutierrez <gutierrez08rom@gmail.com>
2025-07-10 01:56:07 +0200

    changement de la facon d'afficher les projet pour coller au demande du projet

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -8,14 +8,16 @@ const Projects = () => {
     {
       title: "Kasa",
       description: "Site de location de logements",
+      detailedDescription: "Développement d'une application web de location d'appartements entre particuliers. Création d'une interface utilisateur moderne avec React et Sass, implémentation d'un système de navigation avec React Router pour afficher les détails des appartements et gérer les erreurs de navigation.",
       technologies: ["React", "React Router", "Sass"],
       image: "/kasa.png",
       github: "https://github.com/romain2008-dot/kasa-p6",
       live: "https://kasa-p6-drab.vercel.app/"
     },
     {
-      title: "Sophie bluel",
-      description: "Portfolio de sophie bluel",
+      title: "Sophie Bluel",
+      description: "Portfolio de photographe",
+      detailedDescription: "Création d'un site web pour Sophie Bluel, photographe d'art. Développement d'une galerie dynamique avec filtrage par catégories et mise en place d'un système de navigation fluide pour présenter les œuvres photographiques.",
       technologies: ["HTML", "CSS", "JavaScript"],
       image: "./sophie-bluel.png",
       github: "https://github.com/romain2008-dot/sophie-bluel",
@@ -24,7 +26,8 @@ const Projects = () => {
     {
       title: "ArgentBank",
       description: "Application bancaire",
-      technologies: ["React", "React Router", "Redux" ],
+      detailedDescription: "Développement d'une application bancaire avec système de connexion et gestion de profil utilisateur. Création de l'interface utilisateur, implémentation de l'authentification et possibilité de modifier le nom d'utilisateur. Préparation de la documentation Swagger pour l'API future de gestion des transactions.",
+      technologies: ["React", "React Router", "Redux"],
       image: "./argentbank.png",
       github: "https://github.com/romain2008-dot/argentBank",
       live: "https://argentbank-nu.vercel.app/"
